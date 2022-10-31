f9ed4b550cff9cbfbf2953ff3b9f484749832c84
Andrew Leonard <andrewcleonard@fastmail.com>
2022-10-31 16:06:49 -0700

    WIP schemas

--- db/schema.sql
+++ db/schema.sql
@@ -3,17 +3,27 @@ DROP TABLE IF EXISTS job_titles;
 DROP TABLE IF EXISTS departments;
 
 CREATE TABLE employees (
-    employee_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
     job_title VARCHAR(50) NOT NULL,
-    CONSTRAINT fk_department FOREIGN KEY 
+    CONSTRAINT fk_department FOREIGN KEY REFERENCES 
+
+    PRIMARY KEY(id),
+    
 
 )
 
 CREATE TABLE job_titles (
-    job_title_id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    CONSTRAINT fk_employee FOREIGN KEY (employee_id) REFERENCES employees(id) ON DELETE SET NULL
+    salary INTEGER NOT NULL,
+    job_description TEXT,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) 
 )
-CREATE TABLE departments
+CREATE TABLE departments (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY, 
+    department_name VARCHAR(50) NOT NULL,
+
+)
